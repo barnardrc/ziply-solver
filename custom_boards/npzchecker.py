a220b2f72ccbb2910bf344d4b7f24b9cb35f28ed
Ryan Barnard <barnardr98@gmail.com>
2025-09-22 14:10:13 -0400

    Edge case checks

--- custom_boards/npzchecker.py
+++ custom_boards/npzchecker.py
@@ -6,16 +6,28 @@ Created on Sun Sep 21 15:10:03 2025
 """
 import numpy as np
 
-dims_to_check = '8x8'
-
-# Prints the contents to console as easy copy-paste string format
-try:
-    with np.load(dims_to_check + '.npz', allow_pickle=True) as data:
-        # Reconstruct the set from the loaded arrays
-        for arr in data.values():
-            arr = np.array2string(arr, separator=', ')
-            print(arr)
+def main():
+    dims_to_check = input("Type dimensions to pull -> (dim1)x(dim2): ")
+    
+    
+    # Prints the contents to console as easy copy-paste string format
+    try:
+        with np.load(dims_to_check + '.npz', allow_pickle=True) as data:
+            
+            # Reconstruct the set from the loaded arrays
+            if len(data.values()) > 0:
+                for i, arr in enumerate(data.values()):
+                    arr = np.array2string(arr, separator=', ')
+                    print(f"\nBoard {i+1}:")
+                    print(arr)
+                    
+            else:
+                print("- No boards of that size exist.")
+            
+            
+    except FileNotFoundError:
+        # No file found
+        print(f"{dims_to_check}.npz does not exist.")
         
-except FileNotFoundError:
-    # If the file doesn't exist, start with an empty set
-    print("No .npz file found")
+if __name__ == "__main__":
+    main()
